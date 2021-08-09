4d212b63449c9ca759aeff6e6089c5cea58f2f17
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-08-09 20:57:16 +0300

    Initial commit

---
+++ project/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Simpsons} from "./components/simpsons/Simpsons";
+
+function App() {
+  return (
+    <div className="App">
+        <p>the Simpson</p>
+    <Simpsons/>
+    </div>
+  );
+}
+
+export default App;
