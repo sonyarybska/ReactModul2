0e944470e410cb77e24fb43836f8224e9e6696a9
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-08-17 18:25:22 +0300

    new

---
+++ project/src/components/FormOfCarsInfo/FormOfCarsInfo.js
@@ -0,0 +1,43 @@
+import {useState} from "react";
+import {postCars} from "../../services/car.service";
+import './FormOfCarsInfo.css';
+
+export function FormOfCarsInfo() {
+    let [value, setValue] = useState({model: '', price: '', year: ''});
+    let [err, setErr] = useState(['no errors'])
+
+    let myForm = (e) => {
+        e.preventDefault();
+        let arr = [];
+        postCars(value).then(json => {
+            for (const item in json) {
+                if (typeof json[item] === "object") {
+                    arr.push(json[item])
+                    setErr(arr.flat(1));
+                } else {
+                    setErr(['no errors']);
+                }
+            }
+        })
+    };
+
+
+    let formChange = (e) => {
+        setValue({...value, [e.target.name]: e.target.value});
+    }
+
+    return (
+        <div className={'content'}>
+            <form onSubmit={myForm}>
+                <input name={'model'} value={value.model} onChange={formChange} type="text"/>
+                <input name={'price'} value={value.price} onChange={formChange} type="number"/>
+                <input name={'year'} value={value.year} onChange={formChange} type="number"/>
+                <button>Save</button>
+            </form>
+
+            <div className={'error-box'}>
+                {err.map(value => <p>{value}</p>)}
+            </div>
+        </div>
+    )
+}
