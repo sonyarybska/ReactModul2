c87bf4e32ff81bfeb3f75786f858e6c1c8553e3a
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-08-26 01:42:19 +0300

    new

--- project/src/components/counter/Counter.js
+++ project/src/components/counter/Counter.js
@@ -7,6 +7,11 @@
 import {useReducer} from "react";
 import reducer from "../../reducers/reduce.counter";
 
+////////стоврити об'єкт {a:0,b:0,c:0}
+// // // // Стоврити 6 кнопок.
+// // // // по дві на кожен з параметрів об'єкту.
+// // // // одна кнопка збільшує значення , інша зменшує
+// // // // логіку реалізувати через reducer
 
 export function Counter() {
     let [state, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
