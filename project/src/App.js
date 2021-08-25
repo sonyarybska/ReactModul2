6b9bd7ce491b27273129362b8fec23f75bf953ef
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-08-26 01:43:16 +0300

    new

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import {Counter} from "./components/counter/Counter";
 
+////є об'єкт, значення якого 0
+// // Стоврити 2 кнопки.
+// // Перша збільшує   значення на 10
+// // Друга зменшує на 2.
+// // Зміну стану та виведення реалізувати через reducer
 
 function App() {
     return (
